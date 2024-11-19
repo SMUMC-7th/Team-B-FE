851715ce730d536a50a9265dbbb1069978b61c5f
yang dong won <didehddnjs89@gmail.com>
2024-11-19 18:18:30 +0900

    feature: ootd test code 작성 #20

--- app/src/androidTest/java/com/example/umc_wireframe/ServerTest.kt
+++ app/src/androidTest/java/com/example/umc_wireframe/ServerTest.kt
@@ -1,6 +1,8 @@
 package com.example.umc_wireframe
 
+import android.util.Log
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.umc_wireframe.domain.model.Hashtag
 import com.example.umc_wireframe.domain.model.entity.LoginResultEntity
 import com.example.umc_wireframe.domain.repository.MemberRepository
 import com.example.umc_wireframe.domain.repository.OotdRepository
@@ -33,21 +35,70 @@ class TestServerInstrumented {
     fun test() {
         runBlocking {
             testLoginSuccess()
+
+            if(token != ""){
+                testOotdCheck()
+            }
         }
     }
 
     suspend fun testLoginSuccess() {
-        val loginResponse = memberRepository.postLogin(
-            email = testId,
-            password = testPassword
-        )
+        try {
 
-        val loginResult = loginResponse.result
-        assertNotNull(loginResult)
+            val loginResponse = memberRepository.postLogin(
+                email = testId,
+                password = testPassword
+            )
 
-        loginResult?.accessToken?.let {
-            token = "Bearer $it"
-            println("Login successful. Token: $token")
-        } ?: fail("Login result does not contain a token")
+            val loginResult = loginResponse.result
+            assertNotNull(loginResult)
+
+            loginResult?.accessToken?.let {
+                token = "Bearer $it"
+                Log.d("Login success", "Token: $token")
+            } ?: fail("Login result does not contain a token")
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
+    }
+
+    suspend fun testOotdCheck(){
+        val maxTemp = 10
+        val minTemp = 2
+
+        suspend fun checkOotdData() {
+            val hashTagResponse = ootdRepository.getRecommendedHashtag(
+                authorization = token,
+                maxTemperature = maxTemp,
+                minTemperature = minTemp
+            )
+
+            val pastOotdResponseForTemp = ootdRepository.getOotdPastForTemp(
+                authorization = token,
+                maxTemperature = maxTemp,
+                minTemperature = minTemp
+            )
+
+            val pastOotdResponseForMonth = ootdRepository.getOotdPastForYearMonth(
+                authorization = token,
+                year = 2024,
+                month = 11
+            )
+
+            Log.d("checkOotd success", "$hashTagResponse" +
+                    "\n$pastOotdResponseForTemp" +
+                    "\n$pastOotdResponseForMonth")
+        }
+//        fun postOotdData() {
+//            ootdRepository.postOotd(
+//                authorization = token,
+//                maxTemperature = maxTemp,
+//                minTemperature = minTemp,
+//                hashtags = listOf(Hashtag.ADDED_COAT, Hashtag.COAT, Hashtag.FLEECE),
+//                image =
+//            )
+//        }
+
+        checkOotdData()
     }
 }
