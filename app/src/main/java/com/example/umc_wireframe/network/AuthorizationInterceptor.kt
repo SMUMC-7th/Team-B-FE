c6c1b9ad502021fdb352920a1d08b35747807ae7
yang dong won <didehddnjs89@gmail.com>
2024-11-26 20:14:02 +0900

    feature: 비밀번호 변경 구현

--- app/src/main/java/com/example/umc_wireframe/network/AuthorizationInterceptor.kt
+++ app/src/main/java/com/example/umc_wireframe/network/AuthorizationInterceptor.kt
@@ -8,13 +8,15 @@ import okhttp3.Interceptor
 import okhttp3.Response
 
 class AuthorizationInterceptor(
+    private val isForcast:Boolean
 ) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
         // 네트워크 요청에 ServiceKey 추가
         val newUrl = chain.request().url.newBuilder()
-            .addQueryParameter("ServiceKey", BuildConfig.SHORT_TERM_FORECAST_KEY)
-            .build()
+            .apply {
+                if(isForcast) addQueryParameter("ServiceKey", BuildConfig.SHORT_TERM_FORECAST_KEY)
+            }.build()
 
         // NetworkSettings 싱글톤에서 최신 토큰 가져오기
         val sharedPreferencesManager = SharedPreferencesManager(
--- app/src/main/java/com/example/umc_wireframe/presentation/my/passwordchange/PasswordChangeVerificationViewModel.kt
+++ app/src/main/java/com/example/umc_wireframe/presentation/my/passwordchange/PasswordChangeVerificationViewModel.kt
@@ -1,21 +1,31 @@
 package com.example.umc_wireframe.presentation.my.passwordchange
 
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.umc_wireframe.domain.repository.MemberRepository
 import com.example.umc_wireframe.domain.repository.RepositoryFactory
+import com.example.umc_wireframe.presentation.UmcClothsOfTempApplication
 import kotlinx.coroutines.launch
 
 class PasswordChangeVerificationViewModel : ViewModel() {
     private val memberRepository: MemberRepository = RepositoryFactory.createMemberRepository()
 
     fun verifyPasswordChangeReq() = viewModelScope.launch {
-        memberRepository.postPasswordChange()
+        memberRepository.postPasswordChange().run{
+            Toast.makeText(UmcClothsOfTempApplication.context, "인증번호가 발송되었습니다.", Toast.LENGTH_SHORT).show()
+        }
     }
 
-    fun verifyPasswordChange(verificationCode: String, isSuccess: () -> Unit) = viewModelScope.launch {
-        memberRepository.postPasswordVerify(
-            verificationCode = verificationCode
-        )
-    }
+    fun verifyPasswordChange(verificationCode: String, isSuccess: () -> Unit) =
+        viewModelScope.launch {
+            try {
+                memberRepository.postPasswordVerify(
+                    verificationCode = verificationCode
+                )
+                isSuccess()
+            } catch (e: Exception) {
+                Toast.makeText(UmcClothsOfTempApplication.context, e.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
 }
